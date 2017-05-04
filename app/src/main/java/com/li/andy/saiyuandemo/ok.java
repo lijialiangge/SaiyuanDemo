592321fec52fcb72f962266aa12eefc7c338de6b
Andy Li <46574580@qq.com>
2017-05-04 17:45:54 +0800

    t

---
+++ app/src/main/java/com/li/andy/saiyuandemo/ok.java
@@ -0,0 +1,24 @@
+package com.li.andy.saiyuandemo;
+
+
+public class ok {
+    public String o;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getO() {
+        return o;
+
+    }
+
+    public void setO(String o) {
+        this.o = o;
+    }
+}
