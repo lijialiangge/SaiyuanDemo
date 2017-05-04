e963f20663823f8c6868d3cde0b1d6b4002a3d72
Andy Li <46574580@qq.com>
2017-05-04 15:53:02 +0800

    Initial commit

---
+++ app/src/main/java/layout/viewpagerAdapter.java
@@ -0,0 +1,42 @@
+package layout;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class viewpagerAdapter extends PagerAdapter {
+
+
+    private ArrayList<View> views;
+    private Context context;
+
+
+
+
+    public  viewpagerAdapter(ArrayList<View> views,Context context){
+        this.views=views;
+        this.context=context;
+    }
+
+    public int getCount() {
+        return views.size();
+    }
+
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(views.get(position));
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(views.get(position));
+        return views.get(position);
+    }
+
+    public boolean isViewFromObject(View view, Object object) {
+        return (view==object);
+    }
+}
